0751016ed7aa3986bf20f1693545178261b44c10
Conmcgr <cmcgraw@uchicago.edu>
2025-01-07 16:09:05 -0600

    preprocessing script complete

---
+++ src/preprocess.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import re
+
+df = pd.read_csv("../data/raw/Symptom2Disease.csv")
+
+#input --> natural language patient reported symptoms, output --> diagnosis
+
+df.drop('Index', axis=1, inplace=True)
+df.columns = ['output', 'input']
+df['input'] = df['input'].str.replace(r'[^a-zA-Z0-9\s.,]', '', regex=True)
+df['input'] = df['input'].str.lower()
+df['output'] = df['output'].str.replace(r'[^a-zA-Z0-9\s.,]', '', regex=True)
+df['output'] = df['output'].str.lower()
+
+df.dropna(inplace=True)
+df.drop_duplicates(inplace=True)
+
+print(df.head())
+print(df['output'].value_counts())
+
+df.to_json('../data/cleaned/data.jsonl', orient='records')
+
